9b8c61f54ba4c05749b8d55211ff679e43f919b2
Guest User <Guest@Epicodus-10D.local>
2018-03-05 19:07:59 -0800

    add second biz logic condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
+//biz logic
+var leapYear = function(year) {
+  if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+});
+
+//user interface logic
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
+    //debugger;
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
     $("#result").text(result);
   });
-  var leapYear = function(year) {
-    return false;
-  }
-});
